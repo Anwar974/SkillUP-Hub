790b4597a670bd7d80ed42ef1abb0a488a4f0c77
Anwar974 <nwaara24@gmail.com>
2024-12-24 14:46:19 +0200

    edit index.js

--- src/app.router.js
+++ src/app.router.js
@@ -24,20 +24,25 @@ const initApp = (app,express) => {
 //   preflightContinue: false
 // }));
 
-const allowedOrigins = ["http://localhost:5173","https://skillup-front.onrender.com"];
-    app.use(cors({
-          origin: (origin, callback) => {
-            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-                callback(null, true);
-              } else {
-                callback(new Error('Not allowed by CORS'));
-              }
-          },
-          methods: 'GET,POST,PUT,DELETE,HEAD,PATCH',
-          allowedHeaders: 'Content-Type, Authorization',
-          credentials: true, 
-          preflightContinue: false
-        }));
+const corsOptions = {
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://skillup-front.onrender.com"
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true
+  };
+
+    app.use(cors(corsOptions));
+    app.options('*', cors(corsOptions));
         
     app.get('/', (req,res)=>{
         return res.status(200).json({massage:"success"})
